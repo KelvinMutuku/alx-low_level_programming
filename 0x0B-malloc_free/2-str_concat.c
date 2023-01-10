21d56a88e195e12d01e965d80f1d7607746b90bb
KELVIN_MUTUKU <118681525+KelvinMutuku@users.noreply.github.com>
2023-01-10 11:51:59 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,44 +1,62 @@
-/*
- * File: 2-str_concat.c
- * Auth: Kelvin Mutuku
+#include "main.h"
+/**
+ *_strlen - count arrray
+ *@s: array of elements
+ *Return: i
  */
 
-#include "holberton.h"
-#include <stdlib.h>
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0') /*Count character of string*/
+	{
+		i++;
+	}
+
+	return (i);
+}
 
 /**
- * str_concat - Concatenates two strings.
- * @s1: The string to be concatenated upon.
- * @s2: The string to be concatenated to s1.
- *
- * Return: If concatenation fails - NULL.
- *         Otherwise - a pointer the newly-allocated space in memory
- *                     containing the concatenated strings.
+ *str_concat - back a pointer to array
+ *@s1: Array one
+ *@s2: Array two
+ *Return: Always an array dinamic
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *concat_str;
-	int index, concat_index = 0, len = 0;
+	char *dst;
+	unsigned int i, j, size;
 
+	/*If the array is empty*/
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
 
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
+	/*count size total*/
+	size = (_strlen(s1) + _strlen(s2) + 1);
 
-	concat_str = malloc(sizeof(char) * len);
+	/*malloc*/
+	dst = (char *) malloc(size * sizeof(char));
 
-	if (concat_str == NULL)
+	if (dst == 0)
+	{
 		return (NULL);
+	}
 
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+	/*Concatenate arrays*/
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		*(dst + i) = *(s1 + i);
 
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
+	for (j = 0; *(s2 + j) != '\0'; j++)
+	{
+		*(dst + i) = *(s2 + j);
+		i++;
+	}
 
-	return (concat_str);
+	return (dst);
 }
