f760d1cf37898b02972990e040cc0a0fc435ad7d
KELVIN_MUTUKU <118681525+KelvinMutuku@users.noreply.github.com>
2023-01-10 11:54:40 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,52 +1,43 @@
-/*
- * File: 3-alloc_grid.c
- * Auth: Kelvin Mutuku
- */
-
-#include "holberton.h"
-#include <stdlib.h>
-
+#include "main.h"
 /**
- * alloc_grid - Returns a pointer to a 2-dimensional array of
- *               integers with each element initalized to 0.
- * @width: The width of the 2-dimensional array.
- * @height: The height of the 2-dimensional array.
- *
- * Return: If width <= 0, height <= 0, or the function fails - NULL.
- *         Otherwise - a pointer to the 2-dimensional array of integers.
+ * alloc_grid - allocates a grid, make space and free space
+ * @width: takes in width of grid
+ * @height: height of grid
+ * Return: grid with freed spaces
  */
+
 int **alloc_grid(int width, int height)
 {
-	int **twoD;
-	int hgt_index, wid_index;
+/*Declaring variables*/
+int **grid;
+int i, j;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
 
-	twoD = malloc(sizeof(int *) * height);
+grid = malloc(sizeof(int *) * height); /*malloc*/
 
-	if (twoD == NULL)
-		return (NULL);
+if (grid == NULL)
+{
+return (NULL);
+}
 
-	for (hgt_index = 0; hgt_index < height; hgt_index++)
-	{
-		twoD[hgt_index] = malloc(sizeof(int) * width);
-
-		if (twoD[hgt_index] == NULL)
-		{
-			for (; hgt_index >= 0; hgt_index--)
-				free(twoD[hgt_index]);
-
-			free(twoD);
-			return (NULL);
-		}
-	}
-
-	for (hgt_index = 0; hgt_index < height; hgt_index++)
-	{
-		for (wid_index = 0; wid_index < width; wid_index++)
-			twoD[hgt_index][wid_index] = 0;
-	}
-
-	return (twoD);
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(sizeof(int) * width);
+if (grid[i] == NULL)
+{
+for (i = i - 1; i >= 0; i--)
+{
+free(grid[i]);
+}
+free(grid);
+return (NULL);
+}
+}
+for (i = 0; j < width; j++)
+grid[i][j] = 0;
+return (grid);
 }
