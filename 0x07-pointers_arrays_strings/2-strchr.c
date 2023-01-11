02e1fdd243f7daf880cd1974690bd4bda7a59af5
KELVIN_MUTUKU <118681525+KelvinMutuku@users.noreply.github.com>
2023-01-11 10:04:42 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,9 @@
-#include "holberton.h"
+/*
+ * File: 2-strchr.c
+ * Auth: Kelvin mutuku
+ */
+
+#include "main.h"
 
 /**
  * _strchr - Locates a character in a string.
@@ -6,9 +11,8 @@
  * @c: The character to be located.
  *
  * Return: If c is found - a pointer to the first occurence.
- * If c is not found - NULL.
+ *         If c is not found - NULL.
  */
-
 char *_strchr(char *s, char c)
 {
 	int index;
